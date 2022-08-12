44f1b3f1b6f00fad5d642faae353cacac577124f
eunyechan <glqgkq384@naver.com>
2022-08-12 16:06:05 +0900

    fitst commit

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
+  return <div className="App">hello world</div>;
 }
 
 export default App;
